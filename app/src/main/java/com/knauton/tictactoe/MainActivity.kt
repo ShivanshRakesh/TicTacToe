15ba4c8cd91f884a218677aca4801c08363efc32
ShivanshRakesh <49039969+ShivanshRakesh@users.noreply.github.com>
2019-04-02 16:53:47 +0530

    Minor Changes

--- app/src/main/java/com/knauton/tictactoe/MainActivity.kt
+++ app/src/main/java/com/knauton/tictactoe/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.knauton.tictactoe
 
+//Main Activity
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TableLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
